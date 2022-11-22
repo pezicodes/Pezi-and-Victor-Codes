139eeffc724b00cbddbebd49de332283457a76c7
Enwongo-Abasi Udomah <62923280+pezicodes@users.noreply.github.com>
2022-11-22 01:07:39 +0100

    Hello World

---
+++ Pezi&Victor Project/Assets/Scenes/Test.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    void Start()
+    {
+        print("Hello World, This is Victor ");
+    }
+
+    void Update()
+    {
+       
+    }
+}
