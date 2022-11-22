bc6a807ef03da738b1bf93c82da8714258e7f95d
Enwongo-Abasi Udomah <62923280+pezicodes@users.noreply.github.com>
2022-11-22 02:09:59 +0100

    Final Commit

--- Pezi&Victor Project/Assets/Scenes/Test.cs
+++ Pezi&Victor Project/Assets/Scenes/Test.cs
@@ -2,16 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Test : MonoBehaviour
 {
+
+    public Image progressImageBar;
+
+    public Text progressbarText;
+
+    public int loadingtime = 10;
+
+    float timestart = 0;
+
     void Start()
     {
-        print("Hello World, This is Victor ");
+        progressImageBar.color = Color.cyan;
+        progressbarText.color = Color.cyan;
     }
 
-    void Update()
-    {
-       
+    void Update(){
+    
+        timestart += Time.deltaTime;
+
+        if (timestart > 0)
+        {
+            progressImageBar.fillAmount = timestart / loadingtime;
+
+        }
+
+        else{
+            return;
+        }
+
+        progressbarText.text = ((int)(progressImageBar.fillAmount * 100)).ToString() + "%";
+
     }
+
+
+
 }
